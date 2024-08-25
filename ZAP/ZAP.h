50d4a8ba9dddc64fc9c429a2bb277b5be086ac60
Stephen Muscarella <contact@muscarella.info>
2024-08-24 20:42:12 -0400

    Initial Commit

---
+++ ZAP/ZAP.h
@@ -0,0 +1,18 @@
+//
+//  ZAP.h
+//  ZAP
+//
+//  Created by Stephen Muscarella on 8/24/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ZAP.
+FOUNDATION_EXPORT double ZAPVersionNumber;
+
+//! Project version string for ZAP.
+FOUNDATION_EXPORT const unsigned char ZAPVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZAP/PublicHeader.h>
+
+
